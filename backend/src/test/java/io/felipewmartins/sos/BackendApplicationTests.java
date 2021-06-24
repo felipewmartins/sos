d2698ee1d9fc4780fbb64957a871e0c45bce716b
Felipe Martins <felipewmartins@gmail.com>
2021-06-23 22:56:14 -0300

    Commit inicial

---
+++ backend/src/test/java/io/felipewmartins/sos/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.felipewmartins.sos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
