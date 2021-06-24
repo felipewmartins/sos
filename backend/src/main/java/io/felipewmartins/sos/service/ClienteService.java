940c88a0c3128e2723311e75d31ffdbcf46137ea
Felipe Martins <felipewmartins@gmail.com>
2021-06-23 23:23:07 -0300

    Iniciando implementação de cliente

---
+++ backend/src/main/java/io/felipewmartins/sos/service/ClienteService.java
@@ -0,0 +1,5 @@
+package io.felipewmartins.sos.service;
+
+public interface ClienteService {
+
+}
---
+++ backend/src/main/java/io/felipewmartins/sos/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,10 @@
+package io.felipewmartins.sos.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import io.felipewmartins.sos.service.ClienteService;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+}
